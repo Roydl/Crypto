553a5d45b75a2ad9198e7709cef1aa8d638c15eb
Si13n7 <admin@si13n7.com>
2021-05-15 12:30:20 +0200

    Add test case for custom CRC algorithm.

---
+++ test/ChecksumTests/CrcCustomTests.cs
@@ -0,0 +1,217 @@
+﻿namespace Roydl.Crypto.Test.ChecksumTests
+{
+    using System;
+    using System.IO;
+    using Checksum;
+    using NUnit.Framework;
+
+    internal sealed class Crc32Posix : ChecksumAlgorithm<Crc32Posix>
+    {
+        private static readonly CrcConfig<uint> Current = new(32, 0x04c11db7, uint.MinValue, false, true);
+
+        public Crc32Posix() : base(32) { }
+
+        public Crc32Posix(Stream stream) : this() =>
+            Encrypt(stream);
+
+        public Crc32Posix(byte[] bytes) : this() =>
+            Encrypt(bytes);
+
+        public Crc32Posix(string textOrFile, bool strIsFilePath) : this()
+        {
+            if (strIsFilePath)
+            {
+                EncryptFile(textOrFile);
+                return;
+            }
+            Encrypt(textOrFile);
+        }
+
+        public Crc32Posix(string str) : this() =>
+            Encrypt(str);
+
+        public override void Encrypt(Stream stream)
+        {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+            Current.ComputeHash(stream, out var num);
+            HashNumber = num;
+            RawHash = CryptoUtils.GetByteArray(num, RawHashSize, true);
+        }
+    }
+
+    [TestFixture]
+    [NonParallelizable]
+    [Platform(Include = TestVars.PlatformInclude)]
+    public class CrcCustomTests
+    {
+        private const string AlgorithmName = "Crc32Posix";
+        private const int HashBits = 32;
+        private const int HashSize = 8;
+        private const int RawHashSize = 4;
+        private const string ExpectedTestHash = "1e665426";
+        private const string ExpectedRangeHash = "d17dacbe";
+        private static readonly string TestFilePath = TestVars.GetTempFilePath(AlgorithmName);
+
+        private static readonly TestCaseData[] TestData =
+        {
+            new(TestVarsType.TestStream, ExpectedTestHash),
+            new(TestVarsType.TestBytes, ExpectedTestHash),
+            new(TestVarsType.TestString, ExpectedTestHash),
+            new(TestVarsType.TestFile, ExpectedTestHash),
+            new(TestVarsType.RangeString, ExpectedRangeHash)
+        };
+
+        private static Crc32Posix _instanceDefault, _instanceStream, _instanceByteArray, _instanceString, _instanceFilePath;
+
+        [OneTimeSetUp]
+        public void CreateInstances()
+        {
+            _instanceDefault = new Crc32Posix();
+            using (var ms = new MemoryStream(TestVars.TestBytes))
+                _instanceStream = new Crc32Posix(ms);
+            _instanceByteArray = new Crc32Posix(TestVars.TestBytes);
+            _instanceString = new Crc32Posix(TestVars.TestStr);
+            File.WriteAllBytes(TestFilePath, TestVars.TestBytes);
+            _instanceFilePath = new Crc32Posix(TestFilePath, true);
+        }
+
+        [OneTimeTearDown]
+        public void CleanUpTestFiles()
+        {
+            var dir = Path.GetDirectoryName(TestFilePath);
+            if (dir == null)
+                return;
+            foreach (var file in Directory.GetFiles(dir, $"test-{AlgorithmName}-*.tmp"))
+                File.Delete(file);
+        }
+
+        [Test]
+        [TestCase(HashBits, HashSize, RawHashSize)]
+        [Category("New")]
+        public void InstanceCtor(int hashBits, int hashSize, int rawHashSize)
+        {
+            var instanceDefault = new Crc32Posix();
+            Assert.IsInstanceOf(typeof(Crc32Posix), instanceDefault);
+            Assert.IsInstanceOf(typeof(IChecksumAlgorithm), instanceDefault);
+            Assert.AreNotSame(_instanceDefault, instanceDefault);
+            Assert.AreEqual(hashBits, instanceDefault.HashBits);
+            Assert.AreEqual(hashSize, instanceDefault.HashSize);
+            Assert.AreEqual(rawHashSize, instanceDefault.RawHashSize);
+            Assert.AreEqual(default(ReadOnlyMemory<byte>), instanceDefault.RawHash);
+
+            Crc32Posix instanceStream;
+            using (var ms = new MemoryStream(TestVars.TestBytes))
+                instanceStream = new Crc32Posix(ms);
+            Assert.IsInstanceOf(typeof(Crc32Posix), instanceStream);
+            Assert.IsInstanceOf(typeof(IChecksumAlgorithm), instanceStream);
+            Assert.AreNotSame(instanceDefault, instanceStream);
+            Assert.AreEqual(hashSize, instanceStream.Hash.Length);
+
+            var instanceByteArray = new Crc32Posix(TestVars.TestBytes);
+            Assert.IsInstanceOf(typeof(Crc32Posix), instanceByteArray);
+            Assert.IsInstanceOf(typeof(IChecksumAlgorithm), instanceByteArray);
+            Assert.AreNotSame(instanceStream, instanceByteArray);
+            Assert.AreEqual(hashSize, instanceByteArray.Hash.Length);
+
+            var instanceString = new Crc32Posix(TestVars.TestStr);
+            Assert.IsInstanceOf(typeof(Crc32Posix), instanceString);
+            Assert.IsInstanceOf(typeof(IChecksumAlgorithm), instanceString);
+            Assert.AreNotSame(instanceByteArray, instanceString);
+            Assert.AreEqual(hashSize, instanceString.Hash.Length);
+
+            var instanceFilePath = new Crc32Posix(TestFilePath, true);
+            Assert.IsInstanceOf(typeof(Crc32Posix), instanceFilePath);
+            Assert.IsInstanceOf(typeof(IChecksumAlgorithm), instanceFilePath);
+            Assert.AreNotSame(instanceString, instanceFilePath);
+            Assert.AreEqual(hashSize, instanceFilePath.Hash.Length);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(TestData))]
+        [Category("Method")]
+        public void InstanceEncrypt(TestVarsType varsType, string expectedHash)
+        {
+            switch (varsType)
+            {
+                case TestVarsType.TestStream:
+                    using (var ms = new MemoryStream(TestVars.TestBytes))
+                        _instanceDefault.Encrypt(ms);
+                    break;
+                case TestVarsType.TestBytes:
+                    _instanceDefault.Encrypt(TestVars.TestBytes);
+                    break;
+                case TestVarsType.TestString:
+                    _instanceDefault.Encrypt(TestVars.TestStr);
+                    break;
+                case TestVarsType.TestFile:
+                    _instanceDefault.EncryptFile(TestFilePath);
+                    break;
+                case TestVarsType.RangeString:
+                    _instanceDefault.Encrypt(TestVars.RangeStr);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(varsType), varsType, null);
+            }
+            Assert.AreEqual(expectedHash, _instanceDefault.Hash);
+        }
+
+        [Test]
+        [Category("Method")]
+        public void InstanceEquals()
+        {
+            Assert.AreEqual(ExpectedTestHash, _instanceStream.Hash);
+
+            Assert.IsTrue(_instanceStream.Equals((object)_instanceByteArray));
+            Assert.IsTrue(_instanceStream.Equals(_instanceByteArray));
+
+            Assert.IsTrue(_instanceStream.Equals((object)_instanceString));
+            Assert.IsTrue(_instanceStream.Equals(_instanceString));
+
+            Assert.IsTrue(_instanceStream.Equals((object)_instanceFilePath));
+            Assert.IsTrue(_instanceStream.Equals(_instanceFilePath));
+        }
+
+        [Test]
+        [Category("Method")]
+        public void InstanceGetHashCode()
+        {
+            Assert.AreEqual(_instanceDefault.GetHashCode(), new Crc32Posix().GetHashCode());
+            Assert.AreNotEqual(new Adler32().GetHashCode(), _instanceDefault.GetHashCode());
+            Assert.AreNotEqual(new Crc16().GetHashCode(), _instanceDefault.GetHashCode());
+            Assert.AreNotEqual(new Crc32().GetHashCode(), _instanceDefault.GetHashCode());
+            Assert.AreNotEqual(new Crc64().GetHashCode(), _instanceDefault.GetHashCode());
+            Assert.AreNotEqual(new Md5().GetHashCode(), _instanceDefault.GetHashCode());
+            Assert.AreNotEqual(new Sha1().GetHashCode(), _instanceDefault.GetHashCode());
+            Assert.AreNotEqual(new Sha256().GetHashCode(), _instanceDefault.GetHashCode());
+            Assert.AreNotEqual(new Sha384().GetHashCode(), _instanceDefault.GetHashCode());
+            Assert.AreNotEqual(new Sha512().GetHashCode(), _instanceDefault.GetHashCode());
+        }
+
+        [Test]
+        [Category("Operator")]
+        public void InstanceOperators()
+        {
+            Assert.AreEqual(ExpectedTestHash, _instanceStream.Hash);
+
+            Assert.IsTrue(_instanceStream == _instanceByteArray);
+            Assert.IsTrue(_instanceStream == _instanceString);
+            Assert.IsTrue(_instanceStream == _instanceFilePath);
+
+            Assert.IsFalse(_instanceStream != _instanceByteArray);
+            Assert.IsFalse(_instanceStream != _instanceString);
+            Assert.IsFalse(_instanceStream != _instanceFilePath);
+        }
+
+        [Test]
+        [Category("Method")]
+        public void InstanceToString()
+        {
+            Assert.AreEqual(ExpectedTestHash, _instanceStream.ToString());
+            Assert.AreEqual(ExpectedTestHash, _instanceByteArray.ToString());
+            Assert.AreEqual(ExpectedTestHash, _instanceString.ToString());
+            Assert.AreEqual(ExpectedTestHash, _instanceFilePath.ToString());
+            Assert.AreEqual(ExpectedTestHash.ToUpper(), _instanceStream.ToString(true));
+        }
+    }
+}
