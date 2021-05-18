ca4586e74e8ca93b704b406eddb7ed75beea5017
Si13n7 <admin@si13n7.com>
2021-05-18 16:45:15 +0200

    Fix issue where an expected GUID was incorrect.

--- test/OthersTests.cs
+++ test/OthersTests.cs
@@ -15,17 +15,19 @@
             new(TestVarsType.TestBytes, ChecksumAlgo.Crc32, ChecksumAlgo.Sha256, false, "784dd132-532e-aabd-9574-7a6e0f345e25"),
             new(TestVarsType.QuoteString, ChecksumAlgo.Crc16, ChecksumAlgo.Md5, false, "0000d4b3-c852-cafd-9bcd-6f1412be1539"),
             new(TestVarsType.QuoteString, ChecksumAlgo.Sha1, ChecksumAlgo.Crc64, false, "8cd3f7b5-a99b-d111-a253-d111a253ded5"),
-            new(TestVarsType.QuoteString, ChecksumAlgo.Adler32, ChecksumAlgo.Crc16, true, "{ac4410f7-ac44-10f7-0000-ac4410f70000}"),
+            new(TestVarsType.QuoteString, ChecksumAlgo.Adler32, ChecksumAlgo.Crc16, true, "{ac4410f7-d4b3-0000-0000-d4b300000000}"),
             new(TestVarsType.RangeString, ChecksumAlgo.Crc32, ChecksumAlgo.Sha256, false, "7ad6d652-7fb9-8786-c16c-9d568a6db524")
         };
 
         [Test]
         [TestCase(575792, null, null)]
-        public void CombineHashCodes(int expected, object obj1, object obj2) => Assert.AreEqual(expected, CryptoUtils.CombineHashCodes(obj1, obj2));
+        public void CombineHashCodes(int expected, object obj1, object obj2) => 
+            Assert.AreEqual(expected, CryptoUtils.CombineHashCodes(obj1, obj2));
 
         [Test]
         [TestCase(271354309, 10294120, 68356525)]
-        public void CombineHashCodes(int expected, int hashCode1, int hashCode2) => Assert.AreEqual(expected, CryptoUtils.CombineHashCodes(hashCode1, hashCode2));
+        public void CombineHashCodes(int expected, int hashCode1, int hashCode2) => 
+            Assert.AreEqual(expected, CryptoUtils.CombineHashCodes(hashCode1, hashCode2));
 
         [Test]
         [TestCase(TestVarsType.QuoteString, "75edf6dd-8ffa-edd2-652d-2ef5cd3269ac")]
