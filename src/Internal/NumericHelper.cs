6023dbd7ae2e8270e1b40a33b5278298fc95136d
Si13n7 <admin@si13n7.com>
2021-06-13 16:39:13 +0200

    Shorten long generic type names.

--- src/Internal/NumericHelper.cs
+++ src/Internal/NumericHelper.cs
@@ -8,59 +8,59 @@
 
     internal static class NumericHelper
     {
-        internal static TInteger CreateBitMask<TInteger>(int bitWidth) where TInteger : struct, IComparable, IFormattable
+        internal static TInt CreateBitMask<TInt>(int bitWidth) where TInt : struct, IComparable, IFormattable
         {
             var size = (int)MathF.Ceiling(bitWidth / 8f);
-            switch (0xff.FromTo<int, TInteger>())
+            switch (0xff.FromTo<int, TInt>())
             {
                 case byte x:
                 {
                     var mask = x;
                     for (var i = 1; i < size; i++)
                         mask ^= (byte)(0xff << (8 * i));
-                    return (TInteger)(object)mask;
+                    return (TInt)(object)mask;
                 }
                 case short x:
                 {
                     var mask = x;
                     for (var i = 1; i < size; i++)
                         mask ^= (short)(0xff << (8 * i));
-                    return (TInteger)(object)mask;
+                    return (TInt)(object)mask;
                 }
                 case ushort x:
                 {
                     var mask = x;
                     for (var i = 1; i < size; i++)
                         mask ^= (ushort)(0xff << (8 * i));
-                    return (TInteger)(object)mask;
+                    return (TInt)(object)mask;
                 }
                 case int x:
                 {
                     var mask = x;
                     for (var i = 1; i < size; i++)
                         mask ^= 0xff << (8 * i);
-                    return (TInteger)(object)mask;
+                    return (TInt)(object)mask;
                 }
                 case uint x:
                 {
                     var mask = x;
                     for (var i = 1; i < size; i++)
                         mask ^= 0xffu << (8 * i);
-                    return (TInteger)(object)mask;
+                    return (TInt)(object)mask;
                 }
                 case long x:
                 {
                     var mask = x;
                     for (var i = 1; i < size; i++)
                         mask ^= 0xffL << (8 * i);
-                    return (TInteger)(object)mask;
+                    return (TInt)(object)mask;
                 }
                 case ulong x:
                 {
                     var mask = x;
                     for (var i = 1; i < size; i++)
                         mask ^= 0xffuL << (8 * i);
-                    return (TInteger)(object)mask;
+                    return (TInt)(object)mask;
                 }
                 case BigInteger x:
                 {
@@ -68,17 +68,17 @@
                     BigInteger b = 0xff;
                     for (var i = 1; i < size; i++)
                         mask ^= b << (8 * i);
-                    return (TInteger)(object)mask;
+                    return (TInt)(object)mask;
                 }
                 default:
                     throw new NotSupportedException();
             }
         }
 
-        internal static TInteger ReverseBits<TInteger>(this TInteger value) where TInteger : struct, IComparable, IFormattable
+        internal static TInt ReverseBits<TInt>(this TInt value) where TInt : struct, IComparable, IFormattable
         {
-            // String conversion is slower than shifting bits,
-            // but it more accurate for some unusual bit widths.
+            // string conversion is slower than shifting bits,
+            // but it more accurate for some unusual bit widths
             var bits = value switch
             {
                 sbyte x => Convert.ToString(x, 2),
@@ -99,138 +99,144 @@
             bits = new string(bits.Reverse().ToArray());
             return value switch
             {
-                sbyte => (TInteger)(object)Convert.ToSByte(bits, 2),
-                byte => (TInteger)(object)Convert.ToByte(bits, 2),
-                short => (TInteger)(object)Convert.ToInt16(bits, 2),
-                ushort => (TInteger)(object)Convert.ToUInt16(bits, 2),
-                int => (TInteger)(object)Convert.ToInt32(bits, 2),
-                uint => (TInteger)(object)Convert.ToUInt32(bits, 2),
-                long => (TInteger)(object)Convert.ToInt64(bits, 2),
-                _ => (TInteger)(object)Convert.ToUInt64(bits, 2)
+                sbyte => (TInt)(object)Convert.ToSByte(bits, 2),
+                byte => (TInt)(object)Convert.ToByte(bits, 2),
+                short => (TInt)(object)Convert.ToInt16(bits, 2),
+                ushort => (TInt)(object)Convert.ToUInt16(bits, 2),
+                int => (TInt)(object)Convert.ToInt32(bits, 2),
+                uint => (TInt)(object)Convert.ToUInt32(bits, 2),
+                long => (TInt)(object)Convert.ToInt64(bits, 2),
+                _ => (TInt)(object)Convert.ToUInt64(bits, 2)
             };
         }
 
-        internal static TIntegerOut FromTo<TIntegerIn, TIntegerOut>(this TIntegerIn value) where TIntegerIn : struct, IComparable, IFormattable where TIntegerOut : struct, IComparable, IFormattable
+        internal static TIntTo FromTo<TIntFrom, TIntTo>(this TIntFrom value) where TIntFrom : struct, IComparable, IFormattable where TIntTo : struct, IComparable, IFormattable
         {
-            if (typeof(TIntegerIn) == typeof(TIntegerOut))
-                return (TIntegerOut)(object)value;
+            if (typeof(TIntFrom) == typeof(TIntTo))
+                return (TIntTo)(object)value;
 
-            // unchecked assignment
-            return default(TIntegerOut) switch
+            /* slower and inaccurate when downsizing
+
+                var current = value;
+                return Unsafe.As<TIntFrom, TIntTo>(ref current);
+            */
+
+            // pretty fast unchecked assignment
+            return default(TIntTo) switch
             {
                 sbyte => value switch
                 {
-                    byte x => (TIntegerOut)(object)(sbyte)x,
-                    short x => (TIntegerOut)(object)(sbyte)x,
-                    ushort x => (TIntegerOut)(object)(sbyte)x,
-                    int x => (TIntegerOut)(object)(sbyte)x,
-                    uint x => (TIntegerOut)(object)(sbyte)x,
-                    long x => (TIntegerOut)(object)(sbyte)x,
-                    ulong x => (TIntegerOut)(object)(sbyte)x,
-                    BigInteger x => (TIntegerOut)(object)(sbyte)(x & sbyte.MaxValue),
+                    byte x => (TIntTo)(object)(sbyte)x,
+                    short x => (TIntTo)(object)(sbyte)x,
+                    ushort x => (TIntTo)(object)(sbyte)x,
+                    int x => (TIntTo)(object)(sbyte)x,
+                    uint x => (TIntTo)(object)(sbyte)x,
+                    long x => (TIntTo)(object)(sbyte)x,
+                    ulong x => (TIntTo)(object)(sbyte)x,
+                    BigInteger x => (TIntTo)(object)(sbyte)(x & sbyte.MaxValue),
                     _ => throw new NotSupportedException()
                 },
                 byte => value switch
                 {
-                    sbyte x => (TIntegerOut)(object)(byte)x,
-                    short x => (TIntegerOut)(object)(byte)x,
-                    ushort x => (TIntegerOut)(object)(byte)x,
-                    int x => (TIntegerOut)(object)(byte)x,
-                    uint x => (TIntegerOut)(object)(byte)x,
-                    long x => (TIntegerOut)(object)(byte)x,
-                    ulong x => (TIntegerOut)(object)(byte)x,
-                    BigInteger x => (TIntegerOut)(object)(byte)(x & byte.MaxValue),
+                    sbyte x => (TIntTo)(object)(byte)x,
+                    short x => (TIntTo)(object)(byte)x,
+                    ushort x => (TIntTo)(object)(byte)x,
+                    int x => (TIntTo)(object)(byte)x,
+                    uint x => (TIntTo)(object)(byte)x,
+                    long x => (TIntTo)(object)(byte)x,
+                    ulong x => (TIntTo)(object)(byte)x,
+                    BigInteger x => (TIntTo)(object)(byte)(x & byte.MaxValue),
                     _ => throw new NotSupportedException()
                 },
                 short => value switch
                 {
-                    sbyte x => (TIntegerOut)(object)(short)x,
-                    byte x => (TIntegerOut)(object)(short)x,
-                    ushort x => (TIntegerOut)(object)(short)x,
-                    int x => (TIntegerOut)(object)(short)x,
-                    uint x => (TIntegerOut)(object)(short)x,
-                    long x => (TIntegerOut)(object)(short)x,
-                    ulong x => (TIntegerOut)(object)(short)x,
-                    BigInteger x => (TIntegerOut)(object)(short)(x & short.MaxValue),
+                    sbyte x => (TIntTo)(object)(short)x,
+                    byte x => (TIntTo)(object)(short)x,
+                    ushort x => (TIntTo)(object)(short)x,
+                    int x => (TIntTo)(object)(short)x,
+                    uint x => (TIntTo)(object)(short)x,
+                    long x => (TIntTo)(object)(short)x,
+                    ulong x => (TIntTo)(object)(short)x,
+                    BigInteger x => (TIntTo)(object)(short)(x & short.MaxValue),
                     _ => throw new NotSupportedException()
                 },
                 ushort => value switch
                 {
-                    sbyte x => (TIntegerOut)(object)(ushort)x,
-                    byte x => (TIntegerOut)(object)(ushort)x,
-                    short x => (TIntegerOut)(object)(ushort)x,
-                    int x => (TIntegerOut)(object)(ushort)x,
-                    uint x => (TIntegerOut)(object)(ushort)x,
-                    long x => (TIntegerOut)(object)(ushort)x,
-                    ulong x => (TIntegerOut)(object)(ushort)x,
-                    BigInteger x => (TIntegerOut)(object)(ushort)(x & ushort.MaxValue),
+                    sbyte x => (TIntTo)(object)(ushort)x,
+                    byte x => (TIntTo)(object)(ushort)x,
+                    short x => (TIntTo)(object)(ushort)x,
+                    int x => (TIntTo)(object)(ushort)x,
+                    uint x => (TIntTo)(object)(ushort)x,
+                    long x => (TIntTo)(object)(ushort)x,
+                    ulong x => (TIntTo)(object)(ushort)x,
+                    BigInteger x => (TIntTo)(object)(ushort)(x & ushort.MaxValue),
                     _ => throw new NotSupportedException()
                 },
                 int => value switch
                 {
-                    sbyte x => (TIntegerOut)(object)(int)x,
-                    byte x => (TIntegerOut)(object)(int)x,
-                    short x => (TIntegerOut)(object)(int)x,
-                    ushort x => (TIntegerOut)(object)(int)x,
-                    uint x => (TIntegerOut)(object)(int)x,
-                    long x => (TIntegerOut)(object)(int)x,
-                    ulong x => (TIntegerOut)(object)(int)x,
-                    BigInteger x => (TIntegerOut)(object)(int)(x & int.MaxValue),
+                    sbyte x => (TIntTo)(object)(int)x,
+                    byte x => (TIntTo)(object)(int)x,
+                    short x => (TIntTo)(object)(int)x,
+                    ushort x => (TIntTo)(object)(int)x,
+                    uint x => (TIntTo)(object)(int)x,
+                    long x => (TIntTo)(object)(int)x,
+                    ulong x => (TIntTo)(object)(int)x,
+                    BigInteger x => (TIntTo)(object)(int)(x & int.MaxValue),
                     _ => throw new NotSupportedException()
                 },
                 uint => value switch
                 {
-                    sbyte x => (TIntegerOut)(object)(uint)x,
-                    byte x => (TIntegerOut)(object)(uint)x,
-                    short x => (TIntegerOut)(object)(uint)x,
-                    ushort x => (TIntegerOut)(object)(uint)x,
-                    int x => (TIntegerOut)(object)(uint)x,
-                    long x => (TIntegerOut)(object)(uint)x,
-                    ulong x => (TIntegerOut)(object)(uint)x,
-                    BigInteger x => (TIntegerOut)(object)(uint)(x & uint.MaxValue),
+                    sbyte x => (TIntTo)(object)(uint)x,
+                    byte x => (TIntTo)(object)(uint)x,
+                    short x => (TIntTo)(object)(uint)x,
+                    ushort x => (TIntTo)(object)(uint)x,
+                    int x => (TIntTo)(object)(uint)x,
+                    long x => (TIntTo)(object)(uint)x,
+                    ulong x => (TIntTo)(object)(uint)x,
+                    BigInteger x => (TIntTo)(object)(uint)(x & uint.MaxValue),
                     _ => throw new NotSupportedException()
                 },
                 long => value switch
                 {
-                    sbyte x => (TIntegerOut)(object)(long)x,
-                    byte x => (TIntegerOut)(object)(long)x,
-                    short x => (TIntegerOut)(object)(long)x,
-                    ushort x => (TIntegerOut)(object)(long)x,
-                    int x => (TIntegerOut)(object)(long)x,
-                    uint x => (TIntegerOut)(object)(long)x,
-                    ulong x => (TIntegerOut)(object)(long)x,
-                    BigInteger x => (TIntegerOut)(object)(long)(x & long.MaxValue),
+                    sbyte x => (TIntTo)(object)(long)x,
+                    byte x => (TIntTo)(object)(long)x,
+                    short x => (TIntTo)(object)(long)x,
+                    ushort x => (TIntTo)(object)(long)x,
+                    int x => (TIntTo)(object)(long)x,
+                    uint x => (TIntTo)(object)(long)x,
+                    ulong x => (TIntTo)(object)(long)x,
+                    BigInteger x => (TIntTo)(object)(long)(x & long.MaxValue),
                     _ => throw new NotSupportedException()
                 },
                 ulong => value switch
                 {
-                    sbyte x => (TIntegerOut)(object)(ulong)x,
-                    byte x => (TIntegerOut)(object)(ulong)x,
-                    short x => (TIntegerOut)(object)(ulong)x,
-                    ushort x => (TIntegerOut)(object)(ulong)x,
-                    int x => (TIntegerOut)(object)(ulong)x,
-                    uint x => (TIntegerOut)(object)(ulong)x,
-                    long x => (TIntegerOut)(object)(ulong)x,
-                    BigInteger x => (TIntegerOut)(object)(ulong)(x & ulong.MaxValue),
+                    sbyte x => (TIntTo)(object)(ulong)x,
+                    byte x => (TIntTo)(object)(ulong)x,
+                    short x => (TIntTo)(object)(ulong)x,
+                    ushort x => (TIntTo)(object)(ulong)x,
+                    int x => (TIntTo)(object)(ulong)x,
+                    uint x => (TIntTo)(object)(ulong)x,
+                    long x => (TIntTo)(object)(ulong)x,
+                    BigInteger x => (TIntTo)(object)(ulong)(x & ulong.MaxValue),
                     _ => throw new NotSupportedException()
                 },
                 BigInteger => value switch
                 {
-                    sbyte x => (TIntegerOut)(object)(BigInteger)x,
-                    byte x => (TIntegerOut)(object)(BigInteger)x,
-                    short x => (TIntegerOut)(object)(BigInteger)x,
-                    ushort x => (TIntegerOut)(object)(BigInteger)x,
-                    int x => (TIntegerOut)(object)(BigInteger)x,
-                    uint x => (TIntegerOut)(object)(BigInteger)x,
-                    long x => (TIntegerOut)(object)(BigInteger)x,
-                    ulong x => (TIntegerOut)(object)(BigInteger)x,
+                    sbyte x => (TIntTo)(object)(BigInteger)x,
+                    byte x => (TIntTo)(object)(BigInteger)x,
+                    short x => (TIntTo)(object)(BigInteger)x,
+                    ushort x => (TIntTo)(object)(BigInteger)x,
+                    int x => (TIntTo)(object)(BigInteger)x,
+                    uint x => (TIntTo)(object)(BigInteger)x,
+                    long x => (TIntTo)(object)(BigInteger)x,
+                    ulong x => (TIntTo)(object)(BigInteger)x,
                     _ => throw new NotSupportedException()
                 },
                 _ => throw new NotSupportedException()
             };
         }
 
-        internal static string ToHexStr<TInteger>(this TInteger num, int padding, bool prefix) where TInteger : struct, IComparable, IFormattable
+        internal static string ToHexStr<TInt>(this TInt num, int padding, bool prefix) where TInt : struct, IComparable, IFormattable
         {
             var str = num.ToString("x2", null);
             if (padding > 2)
